56ebee678aa7055b5a1e40829dc322f5fb535158
PsyCode404 <Mohamedguenidi7@gmail.com>
2026-02-25 11:36:35 +0100

    fix(backend): enable CORS to support Flutter Web
    - Installed flask-cors
    
    - Added Flask-Cors to requirements.txt
    
    - Configured CORS in app initialization to prevent CORS policy blocks in Chrome

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask_cors import CORS
 
 from config import config_by_name
 from .extensions import db, jwt, migrate, socketio
@@ -9,6 +10,7 @@ from .api import init_api
 def create_app(config_name):
     os.makedirs(os.path.join(os.getcwd(), 'instance'), exist_ok=True)
     app = Flask(__name__)
+    CORS(app, origins="*")  # Enable CORS for all domains to support Flutter Web
     app.config.from_object(config_by_name[config_name])
 
     db.init_app(app)
